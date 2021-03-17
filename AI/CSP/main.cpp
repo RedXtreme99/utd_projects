5fcc1f029b37f35db4fc2898a96742f35c730449
Basil El-Hindi <basilelhindi@gmail.com>
2021-03-17 18:20:14 -0500

    Created makefile, read and store variables/domains

--- AI/CSP/main.cpp
+++ AI/CSP/main.cpp
@@ -5,19 +5,29 @@
 
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    // Check for correct number of command line inputs
     if(argc != 4)
     {
         cout << "Wrong number of inputs" << endl;
         return -1;
     }
 
+    // Ensure each input is valid
     string varfilename = argv[1];
+    if(varfilename.substr(varfilename.length()-4) != ".var")
+    {
+        cout << "Invalid var file name" << endl;
+        return -2;
+    }
     ifstream varfile(varfilename);
     if(!varfile.is_open())
     {
@@ -25,6 +35,11 @@ int main(int argc, char** argv)
         return -2;
     }
     string confilename = argv[2];
+    if(confilename.substr(confilename.length()-4) != ".con")
+    {
+        cout << "Invalid con file name" << endl;
+        return -2;
+    }
     ifstream confile(confilename);
     if(!confile.is_open())
     {
@@ -37,6 +52,22 @@ int main(int argc, char** argv)
         cout << "Invalid procedure given" << endl;
         return -2;
     }
-    cout << "Variable file: " << varfilename << endl << "Constraint file: " << confilename << endl << "Procedure: " << procedure << endl;
+
+    // Store variables and their domains
+    string line;
+    map<char, vector<int> > varMap;
+    while(getline(varfile, line))
+    {
+        char var = line[0];
+        stringstream ss(line.substr(3));
+        string value;
+        while(ss >> value)
+        {
+            varMap[var].push_back(stoi(value));
+        }
+    }
+
+    // Store constraints as a vector
+
     return 0;
 }
